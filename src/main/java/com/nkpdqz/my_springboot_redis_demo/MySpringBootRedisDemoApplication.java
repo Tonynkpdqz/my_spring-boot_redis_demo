cb4ff326a8d70ff40fd88fce4b7dd1269025aaeb
Nkpdqz <529446386@qq.com>
2020-04-07 14:12:41 +0800

    Initial commit

---
+++ src/main/java/com/nkpdqz/my_springboot_redis_demo/MySpringBootRedisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nkpdqz.my_springboot_redis_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootRedisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootRedisDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nkpdqz/my_springboot_redis_demo/MySpringBootRedisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkpdqz.my_springboot_redis_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootRedisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
